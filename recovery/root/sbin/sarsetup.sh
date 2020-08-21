08f80ddbc0c7ddc7827f681e4820bf3acb2af922
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-21 22:57:02 +0300

    montana: Bind mount SAR fstab for Android 9 SAR-based systems
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ie894f616cb4bad406e9e8a04029ae78ff7ba7e44

---
+++ recovery/root/sbin/sarsetup.sh
@@ -0,0 +1,11 @@
+mkdir /s
+mount -o ro /dev/block/bootdevice/by-name/system /s
+build_prop_path="/s/system/build.prop"
+if [ -f "${build_prop_path}" ]; then
+    system_version=$(grep -i 'ro.build.version.release' "${build_prop_path}"  | cut -f2 -d'=')
+    if [ "${system_version}" == "9" ]; then
+        mount none /etc/twrp_sar.fstab /etc/twrp.fstab bind
+    fi
+fi
+umount /s
+rm -rf /s
