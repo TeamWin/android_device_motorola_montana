49268791ebd2643d0371e34d3d2cc1d1f0f467be
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-22 13:33:27 +0300

    montana: Set system-as-root prop on Android 9 SAR system detection
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I8679d78fba8a38448926a749d33fc5cb2b70d74a

--- recovery/root/sbin/sarsetup.sh
+++ recovery/root/sbin/sarsetup.sh
@@ -5,6 +5,7 @@ if [ -f "${build_prop_path}" ]; then
     system_version=$(grep -i 'ro.build.version.release' "${build_prop_path}"  | cut -f2 -d'=')
     if [ "${system_version}" == "9" ]; then
         mount none /etc/twrp_sar.fstab /etc/twrp.fstab bind
+        setprop ro.build.system_root_image true
     fi
 fi
 umount /s
