406d555221576d43eba65e118f63a7d9f88927dd
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-22 21:38:35 +0300

    montana: Set system-as-root prop to false in all other situations
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ic23d82c62ba359667abb21e51912631b0f4f5a91

--- recovery/root/sbin/sarsetup.sh
+++ recovery/root/sbin/sarsetup.sh
@@ -6,7 +6,11 @@ if [ -f "${build_prop_path}" ]; then
     if [ "${system_version}" == "9" ]; then
         mount -o bind /etc/twrp_sar.fstab /etc/twrp.fstab
         setprop ro.build.system_root_image true
+    else
+        setprop ro.build.system_root_image false
     fi
+else
+    setprop ro.build.system_root_image false
 fi
 umount /s
 rm -rf /s
